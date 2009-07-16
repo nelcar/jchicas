8bc39066071acb9f5eba496a49ec22d0b4da1825
julio.chicas <julio.chicas@bc33da16-7033-11de-8b73-c1edde54a3c7>
2009-07-16 04:44:46 +0000

    Excepcion

---
+++ trunk/StackVacioException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jchicas
+ */
+public class StackVacioException extends Exception {
+
+    public StackVacioExepction(String message) {
+
+        super(message);
+    }
+}
+
+
+
+
