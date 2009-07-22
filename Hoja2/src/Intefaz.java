82c0f7026f407e6f01cbabe4c5110497dfa3cbfb
julio.chicas <julio.chicas@bc33da16-7033-11de-8b73-c1edde54a3c7>
2009-07-22 19:03:51 +0000

    Descripcion:
    Se utilizaron Excepciones, Stacks, listas e Interfaz

---
+++ Hoja2/src/Intefaz.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jchicas
+ */
+public interface Intefaz <E> {
+
+
+    	public void push(E push);
+	public E pop();
+	public boolean isEmpty();
+	public E peek();
+
+}
