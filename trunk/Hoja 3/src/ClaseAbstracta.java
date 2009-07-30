b83684157963de4a303bccf0dfc65416957697af
julio.chicas <julio.chicas@bc33da16-7033-11de-8b73-c1edde54a3c7>
2009-07-30 06:53:43 +0000

    Hoja 3

---
+++ trunk/Hoja 3/src/ClaseAbstracta.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jchicas
+ */
+public abstract class ClaseAbstracta<E> implements Interface
+{
+       public abstract void addFirst(Object e);
+       public abstract E removeLast();
+    
+}
